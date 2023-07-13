3b379eeb7d1c9bf0daeaee104a2a3007d8d9bf48
a-down <136649391+a2downing@users.noreply.github.com>
2023-07-13 09:01:43 -0500

    button searches, saves to history, search history loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 // Global Variable
+var searchButton = $('button')
+var searchInput = $('input')
+const searchEl = $('.search-section')
+let cityName
+
+let searchHistory = [];
 
 
 // When I search for a city
@@ -19,5 +25,40 @@
     // trigger display current weather
     // triger display future weather
 
-  
-  
+
+// display search history
+function displaySearchHistory() {
+  searchHistory = JSON.parse(localStorage.getItem('Search History:'));
+  console.log(searchHistory);
+  if (searchHistory === null) {
+    searchHistory = [];
+  } else {
+    for (i = 0; i < searchHistory.length; i++) {
+      var newButton = $(
+        `<button type="submit" class="d-block btn btn-outline-secondary m-2">${searchHistory[i]}</button>`
+      )
+      searchEl.append(newButton);
+    }
+}}
+displaySearchHistory();
+
+
+// search button event trigger 
+searchButton.on('click', function(event) {
+  event.preventDefault();
+  cityName = searchInput.val();
+  console.log(cityName);
+  saveSearchHistory(cityName);
+})
+
+
+// save search to
+function saveSearchHistory(city) {
+  searchHistory.push(cityName);
+  var searchHistoryJSON = JSON.stringify(searchHistory);
+  localStorage.setItem('Search History:', searchHistoryJSON);
+  var newButton = $(
+    `<button type="submit" class="d-block btn btn-outline-secondary m-2">${cityName}</button>`
+  )
+  searchEl.append(newButton);
+}
