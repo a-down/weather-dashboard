f73aa5a097e1881ed8c0a7a8295285363ded270c
a-down <136649391+a2downing@users.noreply.github.com>
2023-07-13 08:29:28 -0500

    initial JS comments

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Global Variable
+
+
+// When I search for a city
+  // presented with current weather conditions
+  // presented with 5-day future weather conditions
+  // that city is added to the search history
+
+
+
+
+  // event listener
+    // button - make an event listener for all buttons in this div. Then no matte
+    // trigger get city function
+    // trigger save to search history function
+      // create button
+    // trigger get API function
+    // trigger getCityWeather function
+    // trigger display current weather
+    // triger display future weather
+
+  
+  
